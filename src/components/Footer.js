dbb89a293c62f9aff155aa8b507b2040e05ef224
Nadine <nadinekdawod@gmail.com>
2024-01-09 15:08:45 +0100

    styling header and adding name + styling footer and adding icon links

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+
+import { FaSquareGithub, FaLinkedin } from "react-icons/fa6";
+
+
+const Footer = () => {
+    return (
+        <div className="footer_body">
+            <div className="footer_container">
+                    <a href="https://github.com/nadine-dawod" target="_blank"><FaSquareGithub className="footer_icon"/></a>
+                    <a href="https://www.linkedin.com/in/nadine-dawod/" target="_blank"><FaLinkedin className="footer_icon"/></a>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <div className="header_body">
+            <div className="header_container">
+                <p className="logo">N A D I N E</p> 
+                <p className="logo">D A W O D</p>
+            </div>
+        </div>
+    );
+};
+export default Header;
